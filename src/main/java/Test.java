fb412c5a37556e7eb573d024c885859145ce17ed
rounding <rounding@163.com>
2019-06-24 09:24:22 +0800

    add method display

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+    public static void display() {
+        System.out.println("hello,idea");
+    }
 }
